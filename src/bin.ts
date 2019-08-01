64989f0e38761fec0df56fe04165d00ee163ddd8
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-08-02 03:26:10 +0900

    ✨ Add version and description of CLI

--- src/bin.ts
+++ src/bin.ts
@@ -1,16 +1,23 @@
 #!/usr/bin/env node
 
 import program from 'commander';
+import readPkg from 'read-pkg';
 
 import main from './';
 
-program
-    .option('--push', '`git push` the added tag to the remote repository')
-    .parse(process.argv);
+(async () => {
+    const PKG = await readPkg();
 
-main({
-    push: program.push,
-}).catch(error => {
-    process.exitCode = 1;
-    console.error(error instanceof Error ? error.message : error);
-});
+    program
+        .version(PKG.version, '-v, --version')
+        .description(PKG.description || '')
+        .option('--push', '`git push` the added tag to the remote repository')
+        .parse(process.argv);
+
+    main({
+        push: program.push,
+    }).catch(error => {
+        process.exitCode = 1;
+        console.error(error instanceof Error ? error.message : error);
+    });
+})();
