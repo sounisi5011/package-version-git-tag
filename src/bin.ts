260dadd7779505380beae08c957eadc5ff5453a1
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-09-10 11:30:12 +0900

    🎨 Replace readJSONFile() function with require()
    JSON file reading can be substituted with require().
    Since the async/await syntax is no longer required, the contents of the generated file will be smaller.

--- src/bin.ts
+++ src/bin.ts
@@ -1,32 +1,30 @@
 #!/usr/bin/env node
 
 import program from 'commander';
-import path from 'path';
 
 import main from './';
-import { isObject, readJSONFile } from './utils';
+import { isObject } from './utils';
 
-(async () => {
-    const PKG = await readJSONFile(path.join(__dirname, '..', 'package.json'));
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const PKG: unknown = require('../package.json');
 
-    if (isObject(PKG)) {
-        if (typeof PKG.version === 'string') {
-            program.version(PKG.version, '-v, --version');
-        }
-
-        if (typeof PKG.description === 'string') {
-            program.description(PKG.description);
-        }
+if (isObject(PKG)) {
+    if (typeof PKG.version === 'string') {
+        program.version(PKG.version, '-v, --version');
     }
 
-    program
-        .option('--push', '`git push` the added tag to the remote repository')
-        .parse(process.argv);
+    if (typeof PKG.description === 'string') {
+        program.description(PKG.description);
+    }
+}
 
-    main({
-        push: program.push,
-    }).catch(error => {
-        process.exitCode = 1;
-        console.error(error instanceof Error ? error.message : error);
-    });
-})();
+program
+    .option('--push', '`git push` the added tag to the remote repository')
+    .parse(process.argv);
+
+main({
+    push: program.push,
+}).catch(error => {
+    process.exitCode = 1;
+    console.error(error instanceof Error ? error.message : error);
+});
