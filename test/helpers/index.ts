f10cea996ce2ce191f79f776641b88886a52eeb4
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-11-12 01:14:16 +0900

    ✅ Create a symbolic link to the node_modules directory in another process
    The EPERM: operation not permitted" error can be related to a Windows specific file lock.
    Therefore, create a symbolic link in another process.

--- test/helpers/index.ts
+++ test/helpers/index.ts
@@ -2,6 +2,8 @@ import fs from 'fs';
 import path from 'path';
 import { promisify } from 'util';
 
+import { execFileAsync } from '../../src/utils';
+
 const symlinkAsync = promisify(fs.symlink);
 
 export { execFileAsync } from '../../src/utils';
@@ -11,15 +13,26 @@ export const writeFile = promisify(fs.writeFile);
 export async function createSymlink({
     symlinkPath,
     linkTarget,
+    subProcess = false,
 }: {
     symlinkPath: string;
     linkTarget: string;
+    subProcess?: boolean;
 }): Promise<void> {
     const symlinkFullpath = path.resolve(symlinkPath);
     const symlinkTargetPath = path.isAbsolute(linkTarget)
         ? path.relative(path.dirname(symlinkFullpath), linkTarget)
         : linkTarget;
-    await symlinkAsync(symlinkTargetPath, symlinkFullpath);
+    if (subProcess) {
+        await execFileAsync('node', [
+            '-e',
+            "require('fs').symlinkSync(process.argv[1], process.argv[2])",
+            symlinkTargetPath,
+            symlinkFullpath,
+        ]);
+    } else {
+        await symlinkAsync(symlinkTargetPath, symlinkFullpath);
+    }
 }
 
 export function getRandomInt(min: number, max: number): number {
