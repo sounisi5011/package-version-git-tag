24af098571001d40150f9d43d8729fae3d56512f
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-11-11 17:32:21 +0900

    ♻️ Remove unnecessary "export" keywords

--- src/git.ts
+++ src/git.ts
@@ -30,7 +30,7 @@ export async function isHeadTag(tagName: string): Promise<boolean> {
     }
 }
 
-export function genTagCmdArgs(
+function genTagCmdArgs(
     tagName: string,
     message?: string,
     sign: boolean = false,
