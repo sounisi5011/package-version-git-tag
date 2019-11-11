c2e4f05c4c49bbb196139e102aa326437c4dfca0
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2019-11-12 00:58:43 +0900

    ⏪ Revert "✅ Change the permissions of symbolic links to the node_modules directory"
    This reverts commit f7a8fc7526b325ac9b44b38f6e240fcf9a5d8bf9.

--- test/helpers/index.ts
+++ test/helpers/index.ts
@@ -7,28 +7,19 @@ const symlinkAsync = promisify(fs.symlink);
 export { execFileAsync } from '../../src/utils';
 
 export const writeFile = promisify(fs.writeFile);
-export const lchmod = promisify(
-    // eslint-disable-next-line node/no-deprecated-api
-    typeof fs.lchmod === 'function' ? fs.lchmod : fs.chmod,
-);
 
 export async function createSymlink({
     symlinkPath,
     linkTarget,
-    mode,
 }: {
     symlinkPath: string;
     linkTarget: string;
-    mode?: number | string;
 }): Promise<void> {
     const symlinkFullpath = path.resolve(symlinkPath);
     const symlinkTargetPath = path.isAbsolute(linkTarget)
         ? path.relative(path.dirname(symlinkFullpath), linkTarget)
         : linkTarget;
     await symlinkAsync(symlinkTargetPath, symlinkFullpath);
-    if (mode !== undefined) {
-        await lchmod(symlinkFullpath, mode);
-    }
 }
 
 export function getRandomInt(min: number, max: number): number {
