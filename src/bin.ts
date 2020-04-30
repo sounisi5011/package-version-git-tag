313837f0d359079b66db1640bc36bcda3c6bcf11
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2020-04-30 09:51:38 +0900

    🎨 Use cac package instead of commander package
    Changed CLI script to code that uses cac package.

--- src/bin.ts
+++ src/bin.ts
@@ -1,33 +1,63 @@
 #!/usr/bin/env node
 
-import program from 'commander';
+import { cac } from 'cac';
 
 import main from './';
 import { isObject } from './utils';
 
+const cli = cac();
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const PKG: unknown = require('../package.json');
 
+let description = '';
 if (isObject(PKG)) {
     if (typeof PKG.version === 'string') {
-        program.version(PKG.version, '-v, --version');
+        cli.version(PKG.version, '-V, -v, --version');
     }
 
     if (typeof PKG.description === 'string') {
-        program.description(PKG.description);
+        description = PKG.description;
     }
 }
+cli.help(
+    description
+        ? (sections) => {
+              sections.splice(1, 0, { body: description });
+          }
+        : undefined,
+);
 
-program
-    .option('--push', '`git push` the added tag to the remote repository')
-    .option('--verbose', 'show details of executed git commands')
-    .option('-n, --dry-run', 'perform a trial run with no changes made')
-    .parse(process.argv);
+cli.option('--push', '`git push` the added tag to the remote repository');
+cli.option('--verbose', 'show details of executed git commands');
+cli.option('-n, --dry-run', 'perform a trial run with no changes made');
+
+if (cli.commands.length <= 0) {
+    cli.usage('[options]');
+}
+
+const { options } = cli.parse();
+if (options.version || options.help) {
+    process.exit();
+}
+
+const unknownOptions = Object.keys(options).filter(
+    (name) => name !== '--' && !cli.globalCommand.hasOption(name),
+);
+if (unknownOptions.length > 0) {
+    process.exitCode = 1;
+    console.error(
+        `unknown ${unknownOptions.length > 1 ? 'options' : 'option'}: ` +
+            unknownOptions
+                .map((name) => (/^[^-]$/.test(name) ? `-${name}` : `--${name}`))
+                .join(' '),
+    );
+    process.exit();
+}
 
 main({
-    push: program.push,
-    verbose: program.verbose,
-    dryRun: program.dryRun,
+    push: options.push,
+    verbose: options.verbose,
+    dryRun: options.dryRun,
 }).catch((error) => {
     process.exitCode = 1;
     console.error(error instanceof Error ? error.message : error);
