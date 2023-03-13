20ee1d2a56e8c7206f298332874bad33f63f1c12
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-03-13 13:41:16 +0900

    🔧 Add `vitest.config.ts` file

---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import { configDefaults, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        include: ['test/**/*.ts'],
+        exclude: [...configDefaults.exclude, '**/helpers/**'],
+    },
+});
