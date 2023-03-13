9403b82a7b751fb8e10dc2cff6ebf4fe94afb324
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-03-14 00:36:10 +0900

    🔧 Add the `testTimeout` option for Vitest
    Some tests take longer than 10 seconds to complete.
    The default timeout is 5 seconds, so tests will not succeed.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
 import { configDefaults, defineConfig } from 'vitest/config';
 
+const sec = (s: number): number => s * 1000;
+
 export default defineConfig({
     test: {
         include: ['test/**/*.ts'],
         exclude: [...configDefaults.exclude, '**/helpers/**'],
+        testTimeout: sec(30),
         watchExclude: [
             ...configDefaults.watchExclude,
             'test/{fixtures,tmp}/**',
