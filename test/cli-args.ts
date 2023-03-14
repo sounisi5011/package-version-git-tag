20f7f61e0367fee6e2332db2ae2907d41b5de9ce
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-03-15 07:13:26 +0900

    ✅ Add tests to parse `--push`, `--verbose`, and unknown options

--- test/cli-args.ts
+++ test/cli-args.ts
@@ -173,6 +173,74 @@ describe('--help option', () => {
     );
 });
 
+describe('--push option', () => {
+    const optionNameList = ['--push'];
+    it.each(truthyOptsCases(optionNameList))('%s', (_, options) =>
+        mockStdioRun((mocks) => {
+            expect(
+                parseArgv(['', '', ...options], parseArgvOpts),
+                'the push option should be true',
+            ).toMatchObject({
+                options: {
+                    push: true,
+                },
+            });
+            expect(mocks.stdout).toHaveBeenCalledTimes(0);
+            expect(mocks.stderr).toHaveBeenCalledTimes(0);
+            expect(mocks.log).toHaveBeenCalledTimes(0);
+        }),
+    );
+    it.each(falsyOptsCases(optionNameList))('%s', (_, options) =>
+        mockStdioRun((mocks) => {
+            expect(
+                parseArgv(['', '', ...options], parseArgvOpts),
+                'the push option should be false',
+            ).toMatchObject({
+                options: {
+                    push: false,
+                },
+            });
+            expect(mocks.stdout).toHaveBeenCalledTimes(0);
+            expect(mocks.stderr).toHaveBeenCalledTimes(0);
+            expect(mocks.log).toHaveBeenCalledTimes(0);
+        }),
+    );
+});
+
+describe('--verbose option', () => {
+    const optionNameList = ['--verbose'];
+    it.each(truthyOptsCases(optionNameList))('%s', (_, options) =>
+        mockStdioRun((mocks) => {
+            expect(
+                parseArgv(['', '', ...options], parseArgvOpts),
+                'the verbose option should be true',
+            ).toMatchObject({
+                options: {
+                    verbose: true,
+                },
+            });
+            expect(mocks.stdout).toHaveBeenCalledTimes(0);
+            expect(mocks.stderr).toHaveBeenCalledTimes(0);
+            expect(mocks.log).toHaveBeenCalledTimes(0);
+        }),
+    );
+    it.each(falsyOptsCases(optionNameList))('%s', (_, options) =>
+        mockStdioRun((mocks) => {
+            expect(
+                parseArgv(['', '', ...options], parseArgvOpts),
+                'the verbose option should be false',
+            ).toMatchObject({
+                options: {
+                    verbose: false,
+                },
+            });
+            expect(mocks.stdout).toHaveBeenCalledTimes(0);
+            expect(mocks.stderr).toHaveBeenCalledTimes(0);
+            expect(mocks.log).toHaveBeenCalledTimes(0);
+        }),
+    );
+});
+
 describe('--dry-run option', () => {
     const optionNameList = ['-n', '--dry-run', '--dryRun'];
     it.each(truthyOptsCases(optionNameList))('%s', (_, options) =>
@@ -205,4 +273,50 @@ describe('--dry-run option', () => {
             expect(mocks.log).toHaveBeenCalledTimes(0);
         }),
     );
+});
+
+test('unknown options should be detected', () => {
+    expect(
+        parseArgv(
+            [
+                '',
+                '',
+                '-xy',
+                '--z',
+                '--un-known',
+                '--inValid',
+                '--sameName',
+                '--sameName',
+                '--camelCaseVsKebabCase',
+                '--camel-case-vs-kebab-case',
+                '--kebab-case-vs-camel-case',
+                '--kebabCaseVsCamelCase',
+                '--has-value=hoge',
+                '--',
+                'ignored',
+            ],
+            parseArgvOpts,
+        ).unknownOptions,
+    ).toStrictEqual([
+        // Should return the actual hyphen-minus used, not the inferred hyphen-minus based on option length.
+        '-x',
+        '-y',
+        '--z',
+        // If the kebab-case option is passed, this should be returned.
+        // Note: cac converts the case style of the option name from kebab-case to lowerCamelCase.
+        //       Raw CLI arguments must be parsed to detect options passed.
+        '--un-known',
+        // If the lowerCamelCase option is passed, this should be returned.
+        // Please return the actual option passed without converting lowerCamelCase to kebab-case.
+        '--inValid',
+        // Duplicate options should not be returned.
+        '--sameName',
+        // The same options, differing only in case style, should be returned in the order in which they were actually passed.
+        '--camelCaseVsKebabCase',
+        '--camel-case-vs-kebab-case',
+        '--kebab-case-vs-camel-case',
+        '--kebabCaseVsCamelCase',
+        // Character "=" and after should be ignored.
+        '--has-value',
+    ]);
 });
