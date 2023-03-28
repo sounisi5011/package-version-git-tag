27fd61bb3e228ca5461f26ab4b41fa6c2ea6850a
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-03-28 16:15:23 +0900

    ♻️ Add `detectPackageManagerUsingEnv()` function

--- src/utils/detect-package-manager.ts
+++ src/utils/detect-package-manager.ts
@@ -18,29 +18,54 @@ function isJsPath(
     filepath: string,
 ): filepath is `${string}${(typeof jsFileExtentions)[number]}` {
     return (jsFileExtentions as readonly string[]).includes(
-        // Note: We use the result of the "path.extname()" function instead of the filepath suffix.
-        //       This is because a "suffix starting with a dot" is not always a file extension.
+        // We use the result of the "path.extname()" function instead of the filepath suffix.
+        // This is because a "suffix starting with a dot" is not always a file extension.
+        // For example: path.extname('.js') === ''
         path.extname(filepath),
     );
 }
 
 /**
+ * @see https://github.com/mysticatea/npm-run-all/blob/v4.1.5/lib/run-task.js#L157-L160
+ * @see https://github.com/BendingBender/yarpm/blob/v1.2.0/lib/index.js#L55-L64
+ */
+function detectPackageManagerUsingEnv(): {
+    type: PackageManagerType | undefined;
+    npmPath: string | undefined;
+} {
+    const npmPath = process.env['npm_execpath'];
+    // Conditional expressions `npmPath !== undefined` and `typeof npmPath === 'string'` are not used.
+    // Because empty strings are excluded here.
+    if (npmPath) {
+        const lowerCaseNpmPathBaseName = path.basename(npmPath).toLowerCase();
+        return {
+            type: packageManagerTypeList.find((type) =>
+                lowerCaseNpmPathBaseName.startsWith(type),
+            ),
+            npmPath,
+        };
+    }
+
+    const packageManager = whichPMRuns()?.name;
+    return {
+        type: packageManagerTypeList.find((type) => packageManager === type),
+        npmPath: undefined,
+    };
+}
+
+/**
  * Detects what package manager was used to run this script.
  * @see https://github.com/mysticatea/npm-run-all/blob/v4.1.5/lib/run-task.js#L157-L174
+ * @see https://github.com/BendingBender/yarpm/blob/v1.2.0/lib/index.js#L55-L67
  */
 export function getPackageManagerData(): PackageManagerData {
-    const npmPath = process.env['npm_execpath'];
-    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    const commandName = npmPath || whichPMRuns()?.name || 'npm';
-    const lowerCaseNpmPathBaseName = path.basename(commandName).toLowerCase();
+    const pmEnv = detectPackageManagerUsingEnv();
 
     return {
-        name: packageManagerTypeList.find((type) =>
-            lowerCaseNpmPathBaseName.startsWith(type),
-        ),
+        name: pmEnv.type,
         spawnArgs:
-            typeof npmPath === 'string' && isJsPath(npmPath)
-                ? [process.execPath, [npmPath]]
-                : [commandName, []],
+            pmEnv.npmPath && isJsPath(pmEnv.npmPath)
+                ? [process.execPath, [pmEnv.npmPath]]
+                : [pmEnv.npmPath ?? pmEnv.type ?? 'npm', []],
     };
 }
