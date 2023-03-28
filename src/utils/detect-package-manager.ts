faa0576ddbf72f89b560466c22f2fc69d3156342
Sonishi Izuka <sounisi5011@users.noreply.github.com>
2023-03-28 15:26:57 +0900

    ♻️ Add `PackageManagerType` and `PackageManagerData` types

--- src/utils/detect-package-manager.ts
+++ src/utils/detect-package-manager.ts
@@ -1,21 +1,30 @@
 import path from 'path';
 import whichPMRuns from 'which-pm-runs';
 
+const packageManagerTypeList = [
+    'npm',
+    'yarn',
+    'pnpm',
+] as const satisfies readonly Lowercase<string>[];
+export type PackageManagerType = (typeof packageManagerTypeList)[number];
+
+export interface PackageManagerData {
+    name: PackageManagerType | undefined;
+    spawnArgs: [commandName: string, prefixArgs: string[]];
+}
+
 /**
  * Detects what package manager was used to run this script.
  * @see https://github.com/mysticatea/npm-run-all/blob/v4.1.5/lib/run-task.js#L157-L174
  */
-export function getPackageManagerData(): {
-    name: 'npm' | 'yarn' | 'pnpm' | undefined;
-    spawnArgs: [commandName: string, prefixArgs: string[]];
-} {
+export function getPackageManagerData(): PackageManagerData {
     const npmPath = process.env['npm_execpath'];
     // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
     const commandName = npmPath || whichPMRuns()?.name || 'npm';
     const lowerCaseNpmPathBaseName = path.basename(commandName).toLowerCase();
 
     return {
-        name: (['npm', 'yarn', 'pnpm'] as const).find((type) =>
+        name: packageManagerTypeList.find((type) =>
             lowerCaseNpmPathBaseName.startsWith(type),
         ),
         spawnArgs:
